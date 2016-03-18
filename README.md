fb2161a51e56ad3ab5696cbd38a2b60de238d64b
Vijith Assar <vijith.assar@nytimes.com>
2016-03-18 15:06:04 -0400

    add scripts

---
+++ README.md
@@ -0,0 +1,7 @@
+# Overview
+
+View the data that powers projects built with [d3.js](http://d3js.org) by simply mousing over them.
+
+# Usage
+
+Visit the [announcement post]() and drag the link posted there to your browser bookmarks. Then, when viewing a d3.js project, click the bookmarklet to trigger it, and any time you mouse over an element with bound data, it will be logged to your browser's developer console. To stop logging, reload the page.
---
+++ d3-xray.js
@@ -0,0 +1,64 @@
+(function() {
+  // used to make sure the line break
+  // only fires once per mouseover
+  var trigger;
+  // if d3 isn't available, bail
+  if (!d3) {
+    return;
+  }
+  // listen to every element (!)
+  // probably not performant, but who caressss
+  d3.selectAll('*')
+    .on('mouseenter', function() {
+      var node,
+          data,
+          attributes,
+          tag,
+          selectors,
+          identifier;
+      node = this;
+      trigger = true;
+      data = d3.select(this).datum();
+      // if there's no bound data, exit
+      if (typeof data == 'undefined') {
+        return;
+      }
+      // compile a node identifier by looking
+      // for these attributes and then prefixing
+      // with the associated string
+      attributes = {
+        'id': '#',
+        'class': '.'
+      };
+      // type of node
+      tag = this.tagName;
+      // CSS selectors
+      selectors = Object.keys(attributes)
+        .map(function(attribute) {
+          var value,
+              prefix;
+          prefix = attributes[attribute];
+          value = d3.select(node).attr(attribute);
+          if (value) {
+            return prefix + value.replace(/ /g, prefix);
+          } else {
+            return '';
+          }
+        }).join('');
+      // identifier string
+      identifier = tag + selectors;
+        // log results to console
+        if (console && typeof console.log === 'function') {
+          console.log(identifier, data);
+        }
+    })
+    .on('mouseleave', function() {
+      // log a line break in between mouse actions
+      if (trigger) {
+        if (console && typeof console.log === 'function') {
+          console.log('');
+        }
+        trigger = false;
+      }
+    });
+}).call(this);
